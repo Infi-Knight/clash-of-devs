f06b096e0129aab4ef00a49d1b4eb96757cb7b00
Infi-Knight <soniravi829@gmail.com>
2017-07-12 13:24:46 +0530

    add react router

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,38 @@
 var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
 var Popular = require('./Popular');
+
+// Switch will render only one route at a time
+// So it will check which route we are on currently
+// Finally it will go to 404 page if none of the route matches to our
+
+// We are using exact inside our routes to make sure that the particular
+// component is rendered only if we are on the route that matches exactly
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-      	<Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function () {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
